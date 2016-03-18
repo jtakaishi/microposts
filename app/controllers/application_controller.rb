720493f2e2e4656f4915a4ff7fd54a7bd38f137b
jtakaishi <junji.takaishi@gmail.com>
2016-03-18 05:12:15 +0000

    Correct login operation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,3 +14,8 @@ class ApplicationController < ActionController::Base
     end
   end
 end
+
+def correct_user
+  @user = User.find(params[:id])
+  redirect_to(root_url) unless @user == current_user
+end
